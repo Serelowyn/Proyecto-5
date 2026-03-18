9ab6cbeaf9eb362b0c8e5c5258cdb2255412fde8
Memo <fretn655@gmail.com>
2026-03-18 15:46:55 -0600

    se cargan los archivos .csv

--- Proyecto 5.py
+++ Proyecto 5.py
@@ -7,3 +7,13 @@ from math import factorial
 from scipy import stats
 
 # ----------------- Fin de las importaciones
+
+# ----------------- Se abren los archivos
+
+df_calls = pandas.read_csv(r"C:\\Users\\sasor\\Desktop\\Tripleten\\Sprint 5\\megaline_calls.csv")
+df_internet = pandas.read_csv(r"C:\\Users\\sasor\\Desktop\\Tripleten\\Sprint 5\\megaline_internet.csv")
+df_mensajes = pandas.read_csv(r"C:\\Users\\sasor\\Desktop\\Tripleten\\Sprint 5\\megaline_messages.csv")
+df_plans = pandas.read_csv(r"C:\\Users\\sasor\\Desktop\\Tripleten\\Sprint 5\\megaline_plans.csv")
+df_users = pandas.read_csv(r"C:\\Users\\sasor\\Desktop\\Tripleten\\Sprint 5\\megaline_users.csv")
+
+# ----------------------------
