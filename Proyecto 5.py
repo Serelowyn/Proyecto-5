efe82ce54802bb88c508d8e35d3c0abbf7060fab
Memo <fretn655@gmail.com>
2026-03-18 16:13:08 -0600

    se corrigen los datos y enriquecen. Se convierte de mb a GB para que tenga sentido con la descripcion del problema, se revisa el tipo de dato y se convierten en minusculas las columnas con tipo de dato int

--- Proyecto 5.py
+++ Proyecto 5.py
@@ -18,7 +18,7 @@ df_users = pandas.read_csv(r"C:\\Users\\sasor\\Desktop\\Tripleten\\Sprint 5\\meg
 
 # ----------------------------
 
-# 1.4 TARIFAS (plans)
+# -------------- 1.4 TARIFAS (plans)
 
 # Imprime la información general/resumida sobre el DataFrame de las tarifas
 
@@ -28,5 +28,22 @@ df_plans.info()
 df_plans.isna()
 
 # Imprime una muestra de los datos para las tarifas
+
+print(df_plans.head())
+
+# -------------- 1.5 Corregir datos (plans)
+
+# de mb a gb
+df_plans['gb_per_month_included'] = df_plans['mb_per_month_included'] / 1024
+
+# -------------- 1.6 Enriquecer datos (plans)
+
+#minusculas
+df_plans['plan_name'] = df_plans['plan_name'].str.lower()
+
+#revisar tipo de dato
+df_plans['usd_per_minute'] = df_plans['usd_per_minute'].astype(float)
+df_plans['usd_per_message'] = df_plans['usd_per_message'].astype(float)
+df_plans['usd_per_gb'] = df_plans['usd_per_gb'].astype(float)
 
 print(df_plans.head())
